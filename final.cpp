0ee54ce27453da528e4a948b18e3a891fcc3000b
youngjaychoi <youngjaychoi02@gmail.com>
2024-12-09 20:44:38 -0800

    commit1

--- final.cpp
+++ final.cpp
@@ -6,9 +6,23 @@
 #include <deque>
 #include <queue>
 #include <list>
+using namespace std;
+
+const int INTIIAL_QUUEUE = 3;
+const int SIMULATION_ROUND = 10;
+
+struct Coffeebooth {
+    string custumer;
+    string order;
+    Coffeebooth *next;
+};
+
+
 
 int main() {
+    srand(time(0));
 
+    
 
 
 
