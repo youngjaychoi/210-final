4d71181ee0b77e08a1c62ea40af37a4ffd4c6246
youngjaychoi <youngjaychoi02@gmail.com>
2024-12-09 21:05:21 -0800

    commit3

--- final.cpp
+++ final.cpp
@@ -11,8 +11,8 @@ using namespace std;
 const int INTIIAL_QUUEUE = 3;
 const int SIMULATION_ROUND = 10;
 
-string names[] = {};
-string drinks[] = {};
+string names[] = {"customer1", "customer2", "customer3", "customer4", "customer5"};
+string drinks[] = {"drink1", "drink2", "drink3", "drink4", "drink5"};
 
 struct Coffeebooth {
     string custumer;
@@ -22,10 +22,13 @@ struct Coffeebooth {
 
 Coffeebooth *randomCustomer();
 
-
 int main() {
     srand(time(0));
 
+    Coffeebooth *coffeeHead = nullptr;
+    for (int i = 0; i < INTIIAL_QUUEUE; i++) {
+        Coffeebooth
+    }
     
 
 
@@ -37,8 +40,8 @@ int main() {
 
 Coffeebooth *randomCustomer() {
     Coffeebooth *newCustomer = new Coffeebooth;
-    
-
-
-
+    newCustomer->custumer = names[rand() % 5];
+    newCustomer->order = drinks[rand() % 5];
+    newCustomer->next = nullptr;
+    return newCustomer;
 }
